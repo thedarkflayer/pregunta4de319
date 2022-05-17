2a05c853304511de0e2e821d20c9272b00cb7e9e
thedarkflayer <jlcch789@gmail.com>
2022-05-17 19:02:03 -0400

    subido cuarta pregunta

---
+++ pregunta4.R
@@ -0,0 +1,5 @@
+barajacompleta <- data.frame(
+	"cartas" = c(rep(c("A",2:10,"J","Q","K"),4)),
+	"tipos" = c(rep(c("Corazon", "Diamante", "Trebol", "Espada"),rep(13,4))),
+	"color" = c(rep("rojo",26),rep("negro",26))
+)
